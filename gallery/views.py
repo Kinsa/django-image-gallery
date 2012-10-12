5998230750df67666b353c14b374c4f72c77cf56
Joseph Bergantine <jbergantine@gmail.com>
2012-10-12 16:19:39 -0600

    Update gallery/views.py

--- gallery/views.py
+++ gallery/views.py
@@ -5,11 +5,6 @@ from gallery.models import PhotoSet
 
 
 def gallery_detail(request, slug):
-    if request.user.is_active:
-            can_login = 'True'
-    else:
-        can_login = 'False'
-
     gallery = get_object_or_404(PhotoSet, slug=slug)
 
     return render_to_response('gallery/photoset_detail.html',
